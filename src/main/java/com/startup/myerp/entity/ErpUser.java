f5b29cec997a7eca80d8f2bbe240e93fa5002313
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2025-01-20 22:48:21 +0600

    cont..

--- src/main/java/com/startup/myerp/entity/ErpUser.java
+++ src/main/java/com/startup/myerp/entity/ErpUser.java
@@ -2,11 +2,16 @@ package com.startup.myerp.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "ERP_USER")
 @Data
-public class ErpUser {
+public class ErpUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,4 +20,29 @@ public class ErpUser {
     private String email;
     private String password;
     private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
