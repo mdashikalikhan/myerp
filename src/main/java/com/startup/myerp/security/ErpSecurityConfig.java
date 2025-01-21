3b9e449663aac8f75a52555011de8ed1bc6b9db5
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2025-01-22 00:26:34 +0600

    Continued..

--- src/main/java/com/startup/myerp/security/ErpSecurityConfig.java
+++ src/main/java/com/startup/myerp/security/ErpSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.startup.myerp.security;
 
+import com.startup.myerp.service.ErpUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,4 +17,15 @@ public class ErpSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public UserDetailsService userDetailsService(ErpUserDetailService erpUserDetailService) {
+        return erpUserDetailService;
+    }
+
+
 }
